9bb182e0ef1fc2e0e6f56d45adb8d6c6b7399257
hoangunity <hoangdm.141196@gmail.com>
2021-02-08 13:13:24 +0700

    Creating A Loader With JavaScript

--- script.js
+++ script.js
@@ -24,15 +24,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
-function getRandomNumber(min, max) {
-  return Math.trunc(min + Math.random() * (max - min));
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
 }
 
 // Show New Quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array;;
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -49,11 +59,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  // Set Quote, Hide Loader
+  complete();
   quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
 
   try {
@@ -78,4 +91,3 @@ twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
-// newQuote();
